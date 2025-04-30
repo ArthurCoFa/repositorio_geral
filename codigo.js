9fb487ce7a443a6fc5ca46040abadbfc1fd85e95
Arthur da Costa Farias <arthurdacosta22012007@gmail.com>
2025-04-30 17:53:03 -0300

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -1,21 +1,13 @@
-// Gerar um n aleatório de 1 a 100
-const randomNumber = Math.floor(Math.random() * 100) + 1;
-
-function checkGuess() {
-  // Obter valor digitado pelo jogador
-  const guess = parseInt(document.getElementById('guessInput').value);
-  // Verificar se o palpite está correto
-  if (guess === randomNumber) {
-    showMessage('Parabéns! Você acertou!');
-  } else if (guess < randomNumber)
-  {
-    showMessage('Tente um n maior.');
-  }else{
-    showMessage('Tente um n menor.');
-  }
+function appendValue(value) {
+  document.getElementById('display').value += value;
 }
 
-function showMessage(message) {
-  // Exibir uma mensagem na tela
-  document.getElementById('message').textContent = message;
+function clearDisplay() {
+  document.getElementById('display').value = ' ';
+}
+
+function calculate() {
+  const display = document.getElementById('display');
+  const result = eval(display.value);
+  display.value = result;
 }
