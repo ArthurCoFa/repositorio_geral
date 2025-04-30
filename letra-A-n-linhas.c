45f5636d64673595a187594b463f43dea51e2be7
ArthurCoFa <arthurdacosta22012007@gmail.com>
2025-04-30 17:24:16 -0300

    novo arquivo

---
+++ letra-A-n-linhas.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main(){
+
+    int num, i, c = 1, j;
+
+    setlocale(LC_ALL, "Portuguese");
+
+    printf("Digite um número maior que 5 : ");
+    scanf("%d", &num);
+
+    while(num < 6){
+
+        printf("O número digitado não é maior que 5!\nTente novamente: ");
+        scanf("%d", &num);
+
+    }
+
+    j = num;
+
+    for(i = 0; i < num; num--){
+
+        while(i < num){
+
+            printf(" ");
+            i++;
+        }
+
+        i = 0;
+
+        if(num == j){
+
+            printf("x\n");
+
+        } else{
+
+            printf("x");
+
+            while(i < c){
+
+                if(j/2 == num){
+
+                    printf("x");
+
+                } else{
+
+                    printf(" ");
+
+                }
+
+                i++;
+            }
+
+            i = 0;
+            c += 2;
+
+            printf("x\n");
+        }
+    }
+
+    return 0;
+}
+
+
